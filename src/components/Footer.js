8ac2c139d29667a61e7a2edb98d5ed4d3c2f7be9
Carl Bush <CarlBushs13@gmail.com>
2022-06-17 19:11:05 -0700

    footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,21 @@ function Footer() {
         <footer>
             ___________________FOOOTER___________________
             GitHub URL and Icon
+            <div>
+                <a href="https://github.com/CarlBush" rel="noreferrer" target="_blank">
+                    <img src={require(`../assets/Octocat.jpg`)} alt="github" width="50" height="50" />
+                </a>
+            </div>
+            <div>
+                <a href="https://twitter.com/" rel="noreferrer" target="_blank">
+                    <img src={require(`../assets/Twitter.png`)} alt="twitter" width="50" height="50" />
+                </a>
+            </div>
+            <div>
+                <a href="https://www.linkedin.com/" rel="noreferrer" target="_blank">
+                    <img src={require(`../assets/LinkedIn.png`)} alt="LinkedIn" width="50" height="50" />
+                </a>
+            </div>
             Linkin URL and Icon
             Twitter URL and Icon
         </footer>
